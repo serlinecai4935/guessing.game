4a7053123421c2c7ec1106285ebd316f25e6092d
scai4935 <scai4935@bths.edu>
2018-10-11 14:50:21 -0400

    2

--- src/guessinggame.java
+++ src/guessinggame.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
 
-public class guessinggame {
-    public static void guessNumber(String args[]);
-    Int guess = " ";
-    System.out.print ("Guess my secret number.");
-    guess = input.nextInt();
+public class guessinggame{
+    public static void main(String[]args) {
+//        Scanner input= new Scanner(System.in);
+        System.out.print("Guess my secret number.");
+//        int guess = 1;
+        double x = Math.random();
+        int num = (int)(x*10+1);
+        Scanner input= new Scanner(System.in);
+        int guess = input.nextInt();
+
+        while (guess != num){
+            System.out.println("Guess again");
+            guess = input.nextInt();
+        }
+        System.out.println("You guessed correctly, the number was " + num);
+    }
 }
