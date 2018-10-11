b845ec53b46f36a6a48a6ad673d4e2456ee63505
serlinecai4935 <scai4935@bths.edu>
2018-10-10 22:12:16 -0400

    Initial commit

---
+++ src/guessinggame.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class guessinggame {
+    public static void guessNumber(String args[]);
+    Int guess = " ";
+    System.out.print ("Guess my secret number.");
+    guess = input.nextInt();
+}
